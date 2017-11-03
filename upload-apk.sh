0d3f4df435186a8a6a69c9be3fd280624bf10453
Chris Eze <ezechris30@gmail.com>
2017-11-03 16:14:37 +0100

    Update upload-apk.sh

--- upload-apk.sh
+++ upload-apk.sh
@@ -4,16 +4,16 @@
   cp -R app/build/outputs/apk/app-debug.apk $HOME/android/
   #go to home and setup git  
   cd $HOME
-  git config --global user.email "useremail@domain.com"
-  git config --global user.name "Your Name" 
+  git config --global user.email "ezechris30@gmail.com"
+  git config --global user.name "ChristalEze" 
   #clone the repository in the buildApk folder
-  git clone --quiet --branch=master  https://user-name:$GITHUB_API_KEY@github.com/user-name/repo-name  master > /dev/null
+  git clone --quiet --branch=master  https://ChristalEze:$GITHUB_API_KEY@github.com/ChristalEze/CryptocurrencyConverter  master > /dev/null
   #go into directory and copy data we're interested
   cd master  cp -Rf $HOME/android/* .
   #add, commit and push files
   git add -f .
   git remote rm origin
-  git remote add origin https://user-name:$GITHUB_API_KEY@github.com/user-name/repo-name.git
+  git remote add origin https://ChristalEze:$GITHUB_API_KEY@github.com/ChristalEze/CryptocurrencyConverter.git
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
   git push -fq origin master > /dev/null
